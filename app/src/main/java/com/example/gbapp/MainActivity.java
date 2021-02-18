85f7b2164a0bbd5a9ffbe9f0ca0bc17f91bcd765
Bersserk <bers.developer@gmail.com>
2021-02-18 21:54:35 +0100

    added calculator_activity

--- app/src/main/java/com/example/gbapp/MainActivity.java
+++ app/src/main/java/com/example/gbapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_activity);
+        setContentView(R.layout.calculator_activity);
     }
 }
